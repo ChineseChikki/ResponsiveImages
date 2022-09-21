48d358069c3c7453247cfbbd319f27f25078d4ae
ChineseChikki <chinelsy@gmail.com>
2022-09-21 13:20:16 +0100

    made changes to navbar

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 /* Toggle between adding and removing the "btn" class to nav when the user clicks on the icon */
-'use strict';
+// 'use strict';
 
-const btn = document.getElementById('btn');
-const menu = document.getElementById('mobileMenu');
-btn.addEventListener('click', () => {
-  btn.classList.toggle('open');
-  menu.classList.toggle('open');
-  console.log(btn);
-});
-
-// function btn() {
-//   console.log('alert');
-// }
-// const btn = document.getElementsByClassName('btn')
-// const btn = document.querySelector('.btn')
-// const btn = document.querySelectorAll('btn')
-// const btn = document.getElementById('btn')
+// const btn = document.getElementById('btn');
+// const menu = document.getElementById('mobileMenu');
+// btn.addEventListener('click', () => {
+//   btn.classList.toggle('open');
+//   menu.classList.toggle('open');
+//   console.log(btn);
+// });
